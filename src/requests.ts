0e820185cffa1e2258e1ac1d9238e6711517fe31
Rohini Singh <2000077607@hexaware.com>
2022-11-06 20:12:08 -0600

    codes_updated

--- src/requests.ts
+++ src/requests.ts
@@ -1,35 +1,35 @@
 "use strict";
 import { BASE_URL } from "./constants";
-import axios from 'axios';
+import axios from "axios";
 import { FactDefinition, FactsDefinition } from "./types";
-
-
 
 /**
  * Request cats fact service
  * @return FactDefinition or undefined
  * @FactDefinition fact: string; length: number;
  */
-export const getFact = async (path: string): Promise<FactDefinition | undefined> => {
-    const response = await axios.get(`${BASE_URL}${path}`);
-    if (isResponseValid(response.status)) {
-        return response.data as FactDefinition;
-    }
-    return undefined;
-}
+export const getFact = async (
+  path: string
+): Promise<FactDefinition | undefined> => {
+  const response = await axios.get(`${BASE_URL}${path}`);
+  if (isResponseValid(response.status)) {
+    return response.data as FactDefinition;
+  }
+  return undefined;
+};
 
 /**
  * Request a list of cat facts
  * @return FactsDefinition or undefined
  */
 export const getFactsList = async (): Promise<FactsDefinition | undefined> => {
-    const response = await axios.get(`${BASE_URL}/facts`);
-    if (isResponseValid(response.status)) {
-        return response.data as FactsDefinition;
-    }
-    return undefined;
-}
+  const response = await axios.get(`${BASE_URL}/facts`);
+  if (isResponseValid(response.status)) {
+    return response.data as FactsDefinition;
+  }
+  return undefined;
+};
 
 export const isResponseValid = (status: number) => {
-    return status === 200;
-}
+  return status === 200;
+};
